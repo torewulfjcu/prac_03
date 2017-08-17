d36e6ce777ec1084b5adbe6336c1ca59732f70f4
Tore <tore.hansen@my.jcu.edu.au>
2017-08-17 11:31:57 +1000

    Refactor parts of the program into functions

--- oddName.py
+++ oddName.py
@@ -1,10 +1,26 @@
 """Tore"""
 
-name = input("Enter name: ")
-while not name:
-    print("Try again!")
+def main():
+    name = get_name()
+    letter_frequency = get_frequency()
+    print_name(name, letter_frequency)
+
+
+def print_name(name, letter_frequency):
+    for i in range(0, len(name), letter_frequency):
+        print(name[i])
+
+
+def get_name():
     name = input("Enter name: ")
+    while not name:
+        print("Try again!")
+        name = input("Enter name: ")
+    return name
 
-for i in range(0, len(name), 2):
-    print(name[i])
+def get_frequency():
+    letter_frequency = int(input("Enter frequency: "))
+    return letter_frequency
 
+
+main()
